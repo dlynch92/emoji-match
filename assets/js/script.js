f63359c1d0933df5d2e725c0713f4fb29d1915d9
dlynch92 <daniellynch92@gmail.com>
2024-04-26 14:45:51 +0100

    add basic js scructure

---
+++ assets/js/script.js
@@ -0,0 +1,50 @@
+
+/**
+ * Start the game
+ */
+function startGame {
+
+}
+
+/**
+ * Randomise and allocate emojis to the grid
+ */
+function shuffleEmojis {
+
+}
+
+/**
+ * Handles when a user selects a tile
+ */
+function clickOnEmoji {
+
+}
+
+/**
+ * Check if two selected tiles are a match
+ */
+function checkForMatch {
+
+}
+
+/**
+ * Increments the score if the selected tiles are not a match
+ */
+function incrementScore {
+
+}
+
+/**
+ * Removes the emojis from the grid if the user has found a match
+ */
+function removeEmoji {
+
+}
+
+/**
+ * Hide game area after a game has ended and display the end screen
+ */
+function endGame {
+
+}
+
