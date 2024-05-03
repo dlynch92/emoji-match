9bd431cc9696043ddef2190c931205cb783a56fe
dlynch92 <daniellynch92@gmail.com>
2024-05-03 16:06:35 +0100

    add disable button presses when incorrect match is made

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,7 @@ let score = 0;
 let timer;
 let card1;
 let card2;
+let buttonsDisabled = false;
 
 // for of loop checks for any button presses once the dom content has loaded
 
@@ -102,9 +103,12 @@ selectedCard.classList.toggle('hide-card');
 
 function flipCards(card1, card2){
 
+    toggleButtons()
+
     setTimeout(function() {
         card1.classList.toggle('hide-card');
         card2.classList.toggle('hide-card');
+        toggleButtons()
       }, 2000);
   
 }
@@ -112,6 +116,7 @@ function flipCards(card1, card2){
  * Increments the score if the selected tiles are not a match
  */
 function incrementScore() {
+
     score++;
     document.getElementById("score").innerHTML = score;
 }
@@ -121,9 +126,8 @@ function incrementScore() {
  */
 function removeEmoji(card1, card2) {
 
- 
-    card1.style.opacity = (0.5);
-    card2.style.opacity = (0.5);
+    card1.style.opacity = (0.3);
+    card2.style.opacity = (0.3);
  
  }
 
@@ -131,6 +135,19 @@ function removeEmoji(card1, card2) {
  * Sets whether or not the cards are clickable by the user - need a few seconds each time a pair is selected where they are unable to be clicked.
  */
 function toggleButtons() {
+
+buttonsDisabled = !buttonsDisabled;
+
+for (let i = 0; i < cards.length; i++){
+    if (buttonsDisabled === true){
+        console.log("buttons disabled");
+        cards[i].disabled = true;
+    } else {
+        console.log("buttons enabled");
+        cards[i].disabled = false;
+    }
+    
+}
 
 }
 /**
